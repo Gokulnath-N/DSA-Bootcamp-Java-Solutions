8ddafb608a84caca9bd0e20d1a0b61deba6caaa1
Gokulnath <gokutruns8@gmail.com>
2023-06-25 20:42:53 +0530

    Create Fibonacci.java

---
+++ src/First Java/Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Fibonacci {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int a=0,b=1,c;
+        System.out.print(a+ " " +b+" ");
+        for(int i=2; i<=n;i++)
+        {
+            c=a+b;
+            System.out.print(c +" ");
+            a=b;
+            b=c;
+        }
+        
+    }
+}
